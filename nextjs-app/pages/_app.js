da3742e2d86c2f15f2e5fbdb17d2cac679986eaa
ThirajVS <thirajvs.ugpro@gmail.com>
2025-06-09 11:05:51 +0530

    css changes

--- nextjs-app/pages/_app.js
+++ nextjs-app/pages/_app.js
@@ -1,5 +1,5 @@
 // pages/_app.js
-import '../public/styles.css';  // adjust path if your CSS lives elsewhere
+import '../styles/globals.css';  // adjust path if your CSS lives elsewhere
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
